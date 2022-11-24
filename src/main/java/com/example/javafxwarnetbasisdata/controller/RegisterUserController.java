7f6d15f1f5bbe97ec40e6115a080699e70770e62
fahmirumagutawan <fahmigutawan@gmail.com>
2022-11-24 07:39:28 +0700

    Add register

--- src/main/java/com/example/javafxwarnetbasisdata/controller/RegisterUserController.java
+++ src/main/java/com/example/javafxwarnetbasisdata/controller/RegisterUserController.java
@@ -7,28 +7,30 @@ import com.example.javafxwarnetbasisdata.util.ResponseListener;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.*;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class LoginUserController {
-    public TextField user_login_username;
-    public PasswordField user_login_password;
-    public Button user_login_back_btn;
-    public Label register_klik_disini_btn;
+public class RegisterUserController {
+    public TextField user_register_username;
+    public PasswordField user_register_password;
+    public Button user_register_back_btn;
 
     public void onBackClicked() throws IOException {
-        Stage stage = (Stage) user_login_back_btn.getScene().getWindow();
+        Stage stage = (Stage) user_register_back_btn.getScene().getWindow();
         Parent root = FXMLLoader.load(getClass().getResource("/com/example/javafxwarnetbasisdata/onboard-view.fxml"));
         Scene scene = new CustomScene(root);
         stage.setScene(scene);
     }
 
-    public void onLoginClicked(){
-        Repository.loginUser(
-                user_login_username.getText(),
-                user_login_password.getText(),
+    public void onRegisterClicked() {
+        Repository.registerUser(
+                user_register_username.getText(),
+                user_register_password.getText(),
                 new ResponseListener() {
                     @Override
                     public void onSuccess(Object o) {
@@ -42,12 +44,5 @@ public class LoginUserController {
                     }
                 }
         );
-    }
-
-    public void onRegisterClicked() throws IOException {
-        Stage stage = (Stage) register_klik_disini_btn.getScene().getWindow();
-        Parent root = FXMLLoader.load(getClass().getResource("/com/example/javafxwarnetbasisdata/register-user-view.fxml"));
-        Scene scene = new CustomScene(root);
-        stage.setScene(scene);
     }
 }
